104a72d02ecb6c1c76dd744789401204f620b991
Kier Lagas <rieksagal@gmail.com>
2021-03-22 07:46:50 +0800

    second commit

--- TDD/TDDProjects/src/index.js
+++ TDD/TDDProjects/src/index.js
@@ -1,27 +1,101 @@
-function getAverage(marks){
-  return Math.floor(marks.reduce((x,y) => x + y) / marks.length);
- }
+// Do I get a bonus?
+function bonusTime(salary, bonus) {
+  var increase = salary * 10;
+  var aw = ['£']
+  return bonus ? aw + increase: aw + salary;
+}
+
+
+//Incorrect division method
+// const solve = (x, y) => x / y
+
+// Get the next prime number!
+// function nextPrime(n) {
+//   if ( ++n < 2 ){ return 2 }
+  
+//   for ( let i = 2; i <= Math.sqrt(n); i++ ){
+//     if ( n % i === 0 ){
+//       return nextPrime(n); 
+//     }
+//   }
+//   return n;
+// }
+
+
+//Over the road
+// function overTheRoad(address, n){
+//   return (n * 2) + 1 - address
+// }
+
+
+//Numbers to Letters
+// function switcher(x){
+//   var alphabet = ' zyxwvutsrqponmlkjihgfedcba!? ';
+//   return x.map(aw => alphabet[aw]).join("");
+// }
+
+//Find The deleted number
+// function findDeletedNumber(arr, mixArr) {
+//   for(i = 0; i < arr.length; i++){
+//     if(!mixArr.includes(arr[i])){
+//       return arr[i];
+//     }
+//   }
+//   return 0;
+// }
+
+// Move all Vowels
+// function moveVowel(input) {
+//   var vowels = "aeiou";
+//   var vowel = "";
+//   var consonants = "";
+//   for(i = 0; i < input.length; i++){
+//     if(vowels.includes(input[i])){
+//       vowel += input[i];
+//     }else{
+//       consonants += input[i]
+//     }
+//   }
+//   return consonants + vowel;
+// }
+
+
+// // Sum of Triangular Numbers
+// function sumTriangularNumbers(n) {
+//   var sum = 0, total = 0;
+//   for(let i = 1; i <= n; i++){
+//     sum += i;
+//     total += sum;
+//   }
+//   return total;
+// }
+
+// function getAverage(marks){
+//   return Math.floor(marks.reduce((x,y) => x + y) / marks.length);
+//  }
 
 // Numbers to Letters
-function switcher(x){
-    var newArray = [];
-    var alphabet = " ?!abcdefghijklmnopqrstuvwxyz".split("").reverse();
+// function switcher(x){
+//     var newArray = [];
+//     var alphabet = " ?!abcdefghijklmnopqrstuvwxyz".split("").reverse();
     
-    for(j = 0; j < x.length; j++){
-      for(i = 0; i <= alphabet.length; i++){    
-       if(i == x[j]){
-         newArray.push(alphabet[i - 1]);
-       } 
-      }
-    }
-    return newArray.join("");
-  }
+//     for(j = 0; j < x.length; j++){
+//       for(i = 0; i <= alphabet.length; i++){    
+//        if(i == x[j]){
+//          newArray.push(alphabet[i - 1]);
+//        } 
+//       }
+//     }
+//     return newArray.join("");
+//   }
 
   // module.exports = switcher;
   // module.exports = getAverage;
- 
-  module.exports = {
-    switcher,
-    getAverage
-  }
-  
+  // module.exports = sumTriangularNumbers;
+  // module.exports = moveVowel;
+  // module.exports = findDeletedNumber;
+  // module.exports = switcher;
+  // module.exports = overTheRoad;
+  // module.exports = nextPrime;
+  // module.exports = solve;
+module.exports = bonusTime;
--- TDD/TDDProjects/test/test.js
+++ TDD/TDDProjects/test/test.js
@@ -1,6 +1,155 @@
-let getAverage = require('../src/index');
+let bonusTime = require('../src/index');
 let assert = require('assert');
 
+// Do I get a bonus?
+describe('If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.', function(){
+    it('example #1', function(){
+        let result = bonusTime(10000, true);
+        assert.equal(result, '£100000');
+    });
+    it('example #2', function(){
+        let result = bonusTime(25000, true);
+        assert.equal(result, '£250000');
+    })
+    it('example #3', function(){
+        let result = bonusTime(10000, false);
+        assert.equal(result, '£10000');
+    })
+})
+
+// My head is at the wrong end!
+// describe('Incorrect division method',function(){
+//     it('example #1', function(){
+//         let result = solve(2,1);
+//         assert.equal(result, 2);
+//     });it('example #2', function(){
+//         let result = solve(8,2);
+//         assert.equal(result, 4);
+//     })
+//     it('example #3', function(){
+//         let result = solve(12,4);
+//         assert.equal(result, 3);
+//     })
+// })
+
+
+// Get the next prime
+// describe('Get the next prime', function(){
+//     it('example #1', function(){
+//         let result = nextPrime(0);
+//         assert.equal(result, 2);
+//     })
+//     it('example #2', function(){
+//         let result = nextPrime(1);
+//         assert.equal(result, 2);
+//     })
+//     it('example #3', function(){
+//         let result = nextPrime(2);
+//         assert.equal(result, 3);
+//     })
+// })
+
+//Over the road
+// describe('Given your house number address and length of street n, give the house number on the opposite side of the street.',function(){
+//     it('example #1', function(){
+//         let result = overTheRoad(1,3);
+//         assert.equal(result, 6)
+//     })
+//     it('example #2', function(){
+//         let result = overTheRoad(3,3);
+//         assert.equal(result, 4)
+//     })
+//     it('example #3', function(){
+//         let result = overTheRoad(2,3);
+//         assert.equal(result, 5)
+//     })
+// })
+
+
+//Numbers to Letters
+// The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively
+// describe('Given an array of numbers (in string format), you must return a string',function(){
+//     it('example #1', function(){
+//         let result = switcher(['24', '12', '23', '22', '4', '26', '9', '8']);
+//         assert.equal(result, 'codewars');
+//     })
+//     it('example #2', function(){
+//         let result = switcher(['25','7','8','4','14','23','8','25','23','29','16','16','4']);
+//         assert.equal(result, 'btswmdsbd kkw');
+//     })
+//     it('example #3', function(){
+//         let result = switcher(['4', '24']);
+//         assert.equal(result, 'wc');
+//     })
+// })
+
+//Find the deleted number
+// describe('find the lost number in number sequence', function(){
+//     it('example #1', function(){
+//         let result = findDeletedNumber([1,2,3,4,5], [3,4,1,5]);
+//         assert.equal(result, 2)
+//     });
+//     it('example #2', function(){
+//         let result = findDeletedNumber([1,2,3,4,5,6,7,8,9], [1,9,7,4,6,2,3,8]);
+//         assert.equal(result, 5)
+//     });
+//     it('example #3', function(){
+//         let result = findDeletedNumber([1,2,3,4,5,6,7,8,9], [5,7,6,9,4,8,1,2,3]);
+//         assert.equal(result, 0)
+//     });
+// })
+
+// Move the Vowel
+// describe('Given a string as input, move all of its vowels to the end of the string, in the same order as they were before.',function(){
+//     it('example #1', function(){
+//         let result = moveVowel("day");
+//         assert.equal(result, "dya");
+//     });
+//     it('example #2', function(){
+//         let result = moveVowel("apple");
+//         assert.equal(result, "pplae");
+//     });
+//     it('example #3', function(){
+//         let result = moveVowel("peace");
+//         assert.equal(result, "pceae");
+//     });
+// })
+
+
+// Sum of the triangular Numbers
+// describe('Return the sum of the Triangular Numbers up to and including the nth triangular Numbers.', function(){
+//     it('example #1', function(){
+//         let result = sumTriangularNumbers(6);
+//         assert.equal(result, 56);
+//     });
+//     it('example #2', function(){
+//         let result = sumTriangularNumbers(34);
+//         assert.equal(result, 7140);
+//     });
+//     it('example #3', function(){
+//         let result = sumTriangularNumbers(-291);
+//         assert.equal(result, 0);
+//     });
+// })
+
+
+// Get The Average
+// describe('Return the average of the given array rounded down to its nearest integer.', function(){
+//     it('example #1', function(){
+//         let result = getAverage([2,2,2,2]);
+//         assert.equal(result, 2);
+//     });
+//     it('example #1', function(){
+//         let result = getAverage([1,2,3,4,5,]);
+//         assert.equal(result, 3);
+//     });
+//     it('example #1', function(){
+//         let result = getAverage([1,1,1,1,1,1,1,2]);
+//         assert.equal(result, 1);
+//     });
+// });
+
+// Numbers to Strings
 // describe('The given array of numbers must be return as a string', function(){
 //     it('example #1', function(){
 //         let result = switcher(['24', '12', '23', '22', '4', '26', '9', '8']);
@@ -15,18 +164,3 @@ let assert = require('assert');
 //         assert.equal(result, 'wc');
 //     })
 // });
-
-describe('Return the average of the given array rounded down to its nearest integer.', function(){
-    it('example #1', function(){
-        let result = getAverage([2,2,2,2]);
-        assert.equal(result, 2);
-    });
-    it('example #1', function(){
-        let result = getAverage([1,2,3,4,5,]);
-        assert.equal(result, 3);
-    });
-    it('example #1', function(){
-        let result = getAverage([1,1,1,1,1,1,1,2]);
-        assert.equal(result, 1);
-    });
-});
